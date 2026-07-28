4309c1cd547294bd50155bb52d5b82930ae5edf5
monster27 <451104485@qq.com>
2026-07-28 13:40:15 +0800

    UI小修改

---
+++ Assets/Scripts/View/Detail/Prefab/UI_CollectionPrefab.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_CollectionPrefab : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
