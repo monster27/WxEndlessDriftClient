e98c8a94396537cf65dde0ac37bc14b1f71fd5ab
monster27 <451104485@qq.com>
2026-08-13 16:14:00 +0800

    更改拉取代码bat

--- 获取工程.bat
+++ 获取工程.bat
@@ -1,1 +1,11 @@
-git pull
+@echo off
+chcp 65001 > nul
+echo ========================================
+echo 正在拉取最新代码...
+echo ========================================
+
+git pull --verbose --log --progress --stat
+
+echo.
+echo ========================================
+pause
